c63a4f7c082fca2b3f72e67ec06f30cc049bdad5
EricTian <erictianc@outlook.com>
2024-12-18 22:42:06 +0800

    test

--- hellodojo.py
+++ hellodojo.py
@@ -1,3 +1,10 @@
 from lean_dojo import *
 
-repo = LeanGitRepo('.',)
+repo = LeanGitRepo("https://github.com/EricTianC/leanf_leandemo","demo1")
+theorem = Theorem(repo, "LeanfLeandemo.lean", "hellodojo")
+
+with Dojo(theorem) as (dojo, init_state):
+  print(init_state)
+  result = dojo.run_tac(init_state, "rw [add_assoc, add_comm b, ←add_assoc]")
+  assert isinstance(result, ProofFinished)
+  print(result)
